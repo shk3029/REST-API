5c8bf2ec35407f8ec43a1052dadf47327a7ba14c
shk3029 <shk3029>
2019-09-17 23:22:04 +0900

    Initial commit

---
+++ src/main/java/me/js/rest/RestApplication.java
@@ -0,0 +1,13 @@
+package me.js.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+}
