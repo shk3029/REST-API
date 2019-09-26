d09b4be32a53c820ccbd877c21c9aa4a25019db3
JS <shk3029>
2019-09-26 21:55:17 +0900

    13. Event 생성 API 구현 - 입력값 이외에 에러 발생

--- src/test/java/me/js/rest/events/EventControllerTests.java
+++ src/test/java/me/js/rest/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.js.rest.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.js.rest.events.dto.EventDto;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,6 +39,40 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Sping")
+                .description("REST TEST SPRING")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23 ,12, 10))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24 ,12, 10))
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 25 ,12, 10))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 26 ,12, 10))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2")
+                .build();
+
+        //Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8) // 요청의 본문에 JSON을 담아 보냄
+                .accept(MediaTypes.HAL_JSON) // HAL_JSON으로 응답을 받고 싶다
+                .content(objectMapper.writeValueAsString(event))
+        )  // perform 안에 요청을 적음
+                .andDo(print()) // 응답 상태를 볼 수 있음
+                .andExpect(status().isCreated()) // status().is(201) 원하는 것을 적어도 됨 (응답 기대값)
+                .andExpect(jsonPath("id").exists()) // id가 존재하는지 테스트
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+
+    @Test
+    public void createEvent_bad_request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Sping")
@@ -54,23 +89,16 @@ public class EventControllerTests {
                 .offline(false)
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
-
         //Mockito.when(eventRepository.save(event)).thenReturn(event);
 
 
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON_UTF8) // 요청의 본문에 JSON을 담아 보냄
-                    .accept(MediaTypes.HAL_JSON) // HAL_JSON으로 응답을 받고 싶다
-                    .content(objectMapper.writeValueAsString(event))
-                    )  // perform 안에 요청을 적음
+                .contentType(MediaType.APPLICATION_JSON_UTF8) // 요청의 본문에 JSON을 담아 보냄
+                .accept(MediaTypes.HAL_JSON) // HAL_JSON으로 응답을 받고 싶다
+                .content(objectMapper.writeValueAsString(event))
+        )  // perform 안에 요청을 적음
                 .andDo(print()) // 응답 상태를 볼 수 있음
-                .andExpect(status().isCreated()) // status().is(201) 원하는 것을 적어도 됨 (응답 기대값)
-                .andExpect(jsonPath("id").exists()) // id가 존재하는지 테스트
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
     }
 
 }
